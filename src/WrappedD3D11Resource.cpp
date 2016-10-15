b4c0c5449127d2d61d49529c6c113996b8e79c1e
etnlGD <etnlGD@gmail.com>
2016-10-16 01:19:53 +0800

    1. 部分支持Multisample的RT 2. 处理非Multisample的RT的copy

--- src/WrappedD3D11Resource.cpp
+++ src/WrappedD3D11Resource.cpp
@@ -3,25 +3,21 @@
 
 namespace rdcboost
 {
-	template <typename TDesc, typename TRes, typename Traits>
-	inline static TRes* CopyResource(
-		TRes* pRealRes, ID3D11Device* pOldDevice, ID3D11Device* pNewDevice,
-		const Traits& pCopyTraits)
-	{
-		TDesc myDesc;
-		pRealRes->GetDesc(&myDesc);
 
+	template <typename TDesc, typename TRes, typename Traits>
+	inline static TRes* CreateResourceWithInitialData(
+		TRes* pRealRes, const TDesc& myDesc, 
+		ID3D11Device* pOldDevice, ID3D11DeviceContext* pOldContext,
+		ID3D11Device* pNewDevice, const Traits& pCopyTraits)
+	{
 		TDesc stageDesc = myDesc;
 		stageDesc.BindFlags = 0;
 		stageDesc.CPUAccessFlags = D3D11_CPU_ACCESS_READ;
 		stageDesc.MiscFlags = 0;
 		stageDesc.Usage = D3D11_USAGE_STAGING;
 
-		ID3D11DeviceContext* pOldContext = NULL;
-		pOldDevice->GetImmediateContext(&pOldContext);
-
-		UINT subresCount = pCopyTraits.GetSubresCount(&myDesc);
 		D3D11_SUBRESOURCE_DATA* pInitialData = NULL;
+		UINT subresCount = pCopyTraits.GetSubresCount(&myDesc);
 		TRes* pStageRes = NULL;
 		if (SUCCEEDED(pCopyTraits.CreateResource(pOldDevice, &stageDesc, NULL, &pStageRes)))
 		{
@@ -49,6 +45,7 @@ namespace rdcboost
 			LogError("Create stage resource failed when CopyToDevice");
 		}
 
+
 		TRes* pNewRes = NULL;
 		if (FAILED(pCopyTraits.CreateResource(pNewDevice, &myDesc, pInitialData, &pNewRes)))
 		{
@@ -64,16 +61,48 @@ namespace rdcboost
 			pStageRes->Release();
 		}
 
+		return pNewRes;
+	}
+
+	template <typename TDesc, typename TRes, typename Traits>
+	inline static TRes* CopyResource(
+		TRes* pRealRes, ID3D11Device* pOldDevice, ID3D11Device* pNewDevice,
+		const Traits& pCopyTraits)
+	{
+		TDesc myDesc;
+		pRealRes->GetDesc(&myDesc);
+
+		ID3D11DeviceContext* pOldContext = NULL, *pNewContext = NULL;
+		pOldDevice->GetImmediateContext(&pOldContext);
+		pNewDevice->GetImmediateContext(&pNewContext);
+
+		TRes* pNewRes = NULL;
+		if (pCopyTraits.GetSampleCount(&myDesc) == 1)
+		{
+			pNewRes = CreateResourceWithInitialData(pRealRes, myDesc, 
+													pOldDevice, pOldContext, 
+													pNewDevice, pCopyTraits);
+		}
+		else
+		{
+			LogError("Multisampled texture data transfer is not supported by now."
+					 "The content of the new texture will be black.");
+
+
+			if (FAILED(pCopyTraits.CreateResource(pNewDevice, &myDesc, NULL, &pNewRes)))
+			{
+				LogError("Create multisampled resource failed on new device when CopyToDevice");
+			}
+		}
+		
 		if (pNewRes != NULL)
 		{
 			FLOAT MinLOD = pOldContext->GetResourceMinLOD(pRealRes);
-			ID3D11DeviceContext* pNewContext = NULL;
-			pNewDevice->GetImmediateContext(&pNewContext);
 			pNewContext->SetResourceMinLOD(pNewRes, MinLOD);
-			pNewContext->Release();
 		}
 
 		pOldContext->Release();
+		pNewContext->Release();
 		return pNewRes;
 	}
 
@@ -93,6 +122,11 @@ namespace rdcboost
 		struct CopyTraitsBuffer
 		{
 			UINT GetSubresCount(const D3D11_BUFFER_DESC* desc) const
+			{
+				return 1;
+			}
+
+			UINT GetSampleCount(const D3D11_BUFFER_DESC* desc) const
 			{
 				return 1;
 			}
@@ -128,6 +162,11 @@ namespace rdcboost
 				return desc->ArraySize * desc->MipLevels;
 			}
 
+			UINT GetSampleCount(const D3D11_TEXTURE1D_DESC* desc) const
+			{
+				return 1;
+			}
+
 			HRESULT CreateResource(ID3D11Device* pDevice, const D3D11_TEXTURE1D_DESC *pDesc, 
 								  const D3D11_SUBRESOURCE_DATA *pInitialData, 
 								  ID3D11Texture1D **ppTexture1D) const
@@ -153,20 +192,16 @@ namespace rdcboost
 
 	ID3D11DeviceChild* WrappedD3D11Texture2D::CopyToDevice(ID3D11Device* pNewDevice)
 	{
-		D3D11_TEXTURE2D_DESC myDesc;
-		GetReal()->GetDesc(&myDesc);
-		if (myDesc.SampleDesc.Count != 1)
-		{
-			// multisampled texture can't be created with initial data.
-			LogError("Multisampled texture is not supported by now.");
-			return NULL;
-		}
-
 		struct CopyTraits2D
 		{
 			UINT GetSubresCount(const D3D11_TEXTURE2D_DESC* desc) const
 			{
 				return desc->ArraySize * desc->MipLevels;
+			}
+
+			UINT GetSampleCount(const D3D11_TEXTURE2D_DESC* desc) const
+			{
+				return desc->SampleDesc.Count;
 			}
 
 			HRESULT CreateResource(ID3D11Device* pDevice, const D3D11_TEXTURE2D_DESC *pDesc, 
@@ -184,15 +219,6 @@ namespace rdcboost
 	void WrappedD3D11Texture2D::CopyToDeviceForSwapChainBuffer(ID3D11Device* pNewDevice, 
 															   ID3D11Texture2D* pNewBuffer)
 	{
-		D3D11_TEXTURE2D_DESC myDesc;
-		GetReal()->GetDesc(&myDesc);
-		if (myDesc.SampleDesc.Count != 1)
-		{
-			// multisampled texture can't be created with initial data.
-			LogError("Multisampled texture is not supported by now.");
-			return;
-		}
-
 		struct CopyTraits2D
 		{
 			UINT GetSubresCount(const D3D11_TEXTURE2D_DESC* desc) const
@@ -200,24 +226,53 @@ namespace rdcboost
 				return desc->ArraySize * desc->MipLevels;
 			}
 
+			UINT GetSampleCount(const D3D11_TEXTURE2D_DESC* desc) const
+			{
+				return desc->SampleDesc.Count;
+			}
+
 			HRESULT CreateResource(ID3D11Device* pDevice, const D3D11_TEXTURE2D_DESC *pDesc,
 								   const D3D11_SUBRESOURCE_DATA *pInitialData,
 								   ID3D11Texture2D **ppTexture2D) const
 			{
-				if (pDevice == pNewDevice)
+				if (pDevice != pNewDevice) // hard-fix, means create stage resource.
 				{
-					// TODO_wzq fill buffer with pInitialData
-					*ppTexture2D = pNewBuffer;
-					return S_OK;
-				}
-				else
-				{
+					Assert(pDesc->Usage == D3D11_USAGE_STAGING);
 					return pDevice->CreateTexture2D(pDesc, pInitialData, ppTexture2D);
 				}
+
+				// hard-fix, means create new swapchain buffer on new device.
+				Assert(pNewBuffer != NULL);
+				if (pInitialData != NULL)
+				{
+					Assert(pDesc->SampleDesc.Count == 1);
+					D3D11_TEXTURE2D_DESC tmpTexDesc = *pDesc;
+					tmpTexDesc.Usage = D3D11_USAGE_IMMUTABLE;
+					tmpTexDesc.BindFlags = 0;
+					tmpTexDesc.CPUAccessFlags = 0;
+					tmpTexDesc.MiscFlags = 0;
+					ID3D11Texture2D* pTmpTexture2D = NULL;
+					if (SUCCEEDED(pDevice->CreateTexture2D(pDesc, pInitialData, &pTmpTexture2D)))
+					{
+						ID3D11DeviceContext* pContext = NULL;
+						pDevice->GetImmediateContext(&pContext);
+						pContext->CopyResource(pNewBuffer, pTmpTexture2D);
+						pTmpTexture2D->Release();
+						pContext->Release();
+					}
+					else
+					{
+						LogError("Create temp texture failed, new swapchain buffer will be black.");
+					}
+				}
+
+				*ppTexture2D = pNewBuffer;
+				pNewBuffer = NULL; // reference obtained by ppTexture2D.
+				return S_OK;
 			}
 
 			ID3D11Device* pNewDevice;
-			ID3D11Texture2D* pNewBuffer;
+			mutable ID3D11Texture2D* pNewBuffer;
 		};
 
 		CopyResource<D3D11_TEXTURE2D_DESC>(
@@ -242,6 +297,11 @@ namespace rdcboost
 			UINT GetSubresCount(const D3D11_TEXTURE3D_DESC* desc) const
 			{
 				return desc->MipLevels;
+			}
+
+			UINT GetSampleCount(const D3D11_TEXTURE3D_DESC* desc) const
+			{
+				return 1;
 			}
 
 			HRESULT CreateResource(ID3D11Device* pDevice, const D3D11_TEXTURE3D_DESC *pDesc,
